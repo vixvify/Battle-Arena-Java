5769ed693a6e832eee7ad76438582d76e1edf630
PuvitLnwZa <puwiztubtimngam.newv@gmail.com>
2026-04-22 13:01:38 +0700

    Add character.java

---
+++ character.java
@@ -0,0 +1,36 @@
+abstract class Character {
+    protected String name;
+    protected int hp;
+    protected int attackDamage;
+
+    public Character(String name, int hp, int attackDamage) {
+        this.name = name;
+        this.hp = hp;
+        this.attackDamage = attackDamage;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+        if (hp < 0) hp = 0;
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    // Abstraction
+    public abstract void attack(Character target);
+
+    // Optional skill (override ได้)
+    public void skill(Character target) {
+        System.out.println(name + " has no special skill!");
+    }
+}
